2d08fea7f8c7531e3de863652c4902dcccbb516c
Geo Perez <geovanni.perez@gmail.com>
2015-05-05 12:19:53 -0500

    Branding and new sample

--- scripts/app.js
+++ scripts/app.js
@@ -52,15 +52,21 @@
                         next: 'sample5'
                     },
                     {
-                        title: 'Grouping',
-                        body: 'You can group by one column (we hope improve this later) and also select the columns to show.',
+                        title: 'Print and Export to CSV',
+                        body: 'Easily you can print or export the current view or entire dataset to CSV using client-side only.',
                         key: 'sample5',
                         next: 'sample6'
                     },
                     {
+                        title: 'Grouping',
+                        body: 'You can group by one column (we hope improve this later) and also select the columns to show.',
+                        key: 'sample6',
+                        next: 'sample7'
+                    },
+                    {
                         title: 'Inline editors (read-only)',
                         body: 'You can add inline editors just defining a Save URL and assigning some controls. This demo is read-only, but you can get the idea.',
-                        key: 'sample6',
+                        key: 'sample7',
                         next: null
                     }
                 ];
