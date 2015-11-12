889b15d02a41fa136fc5d5c5290c775e654a43cc
Luis Gonzalez <luis.gonzalez@unosquare.com>
2015-11-12 10:41:49 -0600

    Saving settings outside of the redraw engine.
    Moving summary to a collapsed panel instead of the title itself
    
    A few improvements to the nav buttons

--- Unosquare.Tubular/Javascript/tubular-widget/tubular-directives-widget.js
+++ Unosquare.Tubular/Javascript/tubular-widget/tubular-directives-widget.js
@@ -31,7 +31,6 @@
                             $scope.settings = localStorageService.get($scope.name + "_data");
 
                             $scope.redraw = function () {
-                                var newSettings = {};
                                 angular.forEach($scope.widgets, function(widget) {
                                     var parent = widget.content.parent();
 
@@ -54,10 +53,18 @@
                                             $('<div class="row"></div>').insertAfter(parent).append(parent.find('div.widget-panel').last());
                                         }
                                     }
+                                });
 
+                                $scope.saveSettings();
+                            };
+
+                            $scope.saveSettings = function () {
+                                var newSettings = {};
+                                angular.forEach($scope.widgets, function(widget) {
                                     newSettings[widget.name] = {
-                                        position: widget.pos,
-                                        oneColumn: widget.oneColumn
+                                        position: widget.position,
+                                        oneColumn: widget.oneColumn,
+                                        collapsed: widget.collapsed
                                     };
                                 });
 
@@ -74,8 +81,9 @@
                                         if (!setting) return;
 
                                         widget.oneColumn = setting.oneColumn;
-                                        if (widget.pos !== setting.pos) {
-                                            widget.moveAt(setting.pos);
+                                        widget.collapsed = setting.collapsed;
+                                        if (widget.position !== setting.position) {
+                                            widget.moveAt(setting.position);
                                         }
                                     });
                                 }
@@ -102,27 +110,29 @@
             function() {
                 return {
                     template: '<div class="widget-panel">' +
-                        '<div class="tubular-overlay maximize" ng-show="maximize"></div>' +
-                        '<div ng-class="{ \'col-md-6\': oneColumn, \'col-md-12\': !oneColumn, \'maximize\': maximize}">' +
+                        '<div class="tubular-overlay maximized" ng-show="maximized"></div>' +
+                        '<div ng-class="{ \'col-md-6\': oneColumn, \'col-md-12\': !oneColumn, \'maximized\': maximized}">' +
                             '<div class="panel panel-default">' +
                             '<div class="panel-heading">' +
-                                '<span><strong>{{title}}</strong></span> ' +
-                                '<span ng-show="collapsed">{{summary}}</span>' +
+                                '<span>{{title}}</span> ' +
                                 '<div class="pull-right">' +
-                                    '<button ng-disabled="top || maximize" ng-click="move(-2)" class="btn btn-default btn-xs"  title="{{\'UI_MOVEUP\'| translate}}"><span><i class="glyphicon glyphicon-arrow-up"></i></span></button>' +
-                                    '<button ng-disabled="bottom || maximize" ng-click="move(2)" class="btn btn-default btn-xs"  title="{{\'UI_MOVEDOWN\'| translate}}"><span><i class="glyphicon glyphicon-arrow-down"></i></span></button>' +
-                                    '<button ng-disabled="!oneColumn || !even || maximize" ng-click="move(1)" class="btn btn-default btn-xs"  title="{{\'UI_MOVERIGHT\'| translate}}"><span><i class="glyphicon glyphicon-arrow-right"></i></span></button>' +
-                                    '<button ng-disabled="!oneColumn || even || maximize" ng-click="move(-1)" class="btn btn-default btn-xs"  title="{{\'UI_MOVELEFT\'| translate}}"><span><i class="glyphicon glyphicon-arrow-left"></i></span></button>' +
-                                    '<button ng-hide="!oneColumn || maximize" ng-click="oneColumn = false" class="btn btn-default btn-xs" title="{{\'UI_TWOCOLS\'| translate}}"><span><i class="glyphicon glyphicon-resize-horizontal"></i></span></button>' +
-                                    '<button ng-hide="oneColumn || maximize" ng-click="oneColumn = true" class="btn btn-default btn-xs"  title="{{\'UI_ONECOL\'| translate}}"><span><i class="glyphicon glyphicon-resize-horizontal"></i></span></button>' +
-                                    '<button ng-hide="collapsed" ng-click="collapsed = true" class="btn btn-default btn-xs"  title="{{\'UI_COLLAPSE\'| translate}}"><span><i class="glyphicon glyphicon-triangle-top"></i></span></button>' +
-                                    '<button ng-hide="!collapsed" ng-click="collapsed = false" class="btn btn-default btn-xs"  title="{{\'UI_EXPAND\'| translate}}"><span><i class="glyphicon glyphicon-triangle-bottom"></i></span></button>' +
-                                    '<button ng-hide="maximize" ng-click="maximize = true" class="btn btn-default btn-xs"  title="{{\'UI_MAXIMIZE\'| translate}}"><span><i class="glyphicon glyphicon-resize-full"></i></span></button>' +
-                                    '<button ng-hide="!maximize" ng-click="maximize = false" class="btn btn-default btn-xs"  title="{{\'UI_RESTORE\'| translate}}"><span><i class="glyphicon glyphicon-resize-small"></i></span></button>' +
+                                    '<button ng-hide="maximized" ng-disabled="top" ng-click="move(-2)" class="btn btn-default btn-xs"  title="{{\'UI_MOVEUP\'| translate}}"><span><i class="glyphicon glyphicon-arrow-up"></i></span></button>' +
+                                    '<button ng-hide="maximized" ng-disabled="bottom" ng-click="move(2)" class="btn btn-default btn-xs"  title="{{\'UI_MOVEDOWN\'| translate}}"><span><i class="glyphicon glyphicon-arrow-down"></i></span></button>' +
+                                    '<button ng-hide="maximized" ng-disabled="!oneColumn || !even" ng-click="move(1)" class="btn btn-default btn-xs"  title="{{\'UI_MOVERIGHT\'| translate}}"><span><i class="glyphicon glyphicon-arrow-right"></i></span></button>' +
+                                    '<button ng-hide="maximized" ng-disabled="!oneColumn || even" ng-click="move(-1)" class="btn btn-default btn-xs"  title="{{\'UI_MOVELEFT\'| translate}}"><span><i class="glyphicon glyphicon-arrow-left"></i></span></button>' +
+                                    '<button ng-hide="!oneColumn || maximized" ng-click="oneColumn = false" class="btn btn-default btn-xs" title="{{\'UI_TWOCOLS\'| translate}}"><span><i class="glyphicon glyphicon-resize-horizontal"></i></span></button>' +
+                                    '<button ng-hide="oneColumn || maximized" ng-click="oneColumn = true" class="btn btn-default btn-xs"  title="{{\'UI_ONECOL\'| translate}}"><span><i class="glyphicon glyphicon-resize-horizontal"></i></span></button>' +
+                                    '<button ng-hide="collapsed || maximized" ng-click="collapsed = true" class="btn btn-default btn-xs"  title="{{\'UI_COLLAPSE\'| translate}}"><span><i class="glyphicon glyphicon-menu-up"></i></span></button>' +
+                                    '<button ng-hide="!collapsed || maximized" ng-click="collapsed = false" class="btn btn-default btn-xs"  title="{{\'UI_EXPAND\'| translate}}"><span><i class="glyphicon glyphicon-menu-down"></i></span></button>' +
+                                    '<button ng-hide="maximized" ng-click="maximized = true;" class="btn btn-default btn-xs"  title="{{\'UI_MAXIMIZE\'| translate}}"><span><i class="glyphicon glyphicon-resize-full"></i></span></button>' +
+                                    '<button ng-hide="!maximized" ng-click="maximized = false" class="btn btn-default btn-xs"  title="{{\'UI_RESTORE\'| translate}}"><span><i class="glyphicon glyphicon-resize-small"></i></span></button>' +
                                 '</div>'+
                             '</div>'+
-                            '<div class="panel-body">' +
+                            '<div class="panel-body" ng-hide="collapsed && !maximized">' +
                                 '<ng-transclude></ng-transclude>' +
+                            '</div>' +
+                            '<div class="panel-body" ng-show="collapsed && !maximized">' +
+                                '{{summary}}' +
                             '</div>' +
                         '</div>',
                     restrict: 'E',
@@ -137,32 +147,31 @@
                         $scope.name = $scope.name || 'tbwidget';
                         $scope.oneColumn = true;
                         $scope.collapsed = false;
-                        $scope.maximize = false;
-                        $scope.pos = 0;
+                        $scope.maximized = false;
+                        $scope.position = 0;
                         $scope.content = $element;
                         $scope.container = $scope.$parent.$parent;
 
                         $scope.container.widgets.push($scope);
 
-                        $scope.calculatePositions = function(pos) {
-                            $scope.top = pos === 0 || pos === 1;
-                            $scope.bottom = pos === $scope.container.widgets.length - 1 || pos === $scope.container.widgets.length - 2;
-                            $scope.even = pos % 2 === 0;
-                            $scope.pos = pos;
+                        $scope.calculatePositions = function(position) {
+                            $scope.top = position === 0 || position === 1;
+                            $scope.bottom = position === $scope.container.widgets.length - 1 || position === $scope.container.widgets.length - 2;
+                            $scope.even = position % 2 === 0;
+                            $scope.position = position;
                         };
 
                         $scope.$watch('$parent.widgets', function (val) {
-                            var pos = $scope.container.widgets.indexOf($scope);
-                            $scope.calculatePositions(pos);
+                            var position = $scope.container.widgets.indexOf($scope);
+                            $scope.calculatePositions(position);
                         });
 
-                        $scope.$watch('oneColumn', function() {
+                        $scope.$watch('oneColumn', function () {
                             $scope.container.redraw();
                         });
 
                         $scope.$watch('collapsed', function (v) {
-                            var el = $scope.content.find('.panel-body');
-                            $scope.collapsed ? el.hide() : el.show();
+                            $scope.container.saveSettings();
                         });
 
                         function swapNodes(a, b) {
@@ -173,18 +182,18 @@
                         }
 
                         $scope.move = function(factor) {
-                            $scope.moveAt($scope.pos + factor);
+                            $scope.moveAt($scope.position + factor);
                         };
 
-                        $scope.moveAt = function (pos) {
-                            var tmp = $scope.container.widgets[pos];
+                        $scope.moveAt = function (position) {
+                            var tmp = $scope.container.widgets[position];
                             if (!tmp) return;
 
-                            $scope.container.widgets[pos] = $scope;
-                            $scope.container.widgets[$scope.pos] = tmp;
+                            $scope.container.widgets[position] = $scope;
+                            $scope.container.widgets[$scope.position] = tmp;
 
-                            tmp.calculatePositions($scope.pos);
-                            $scope.calculatePositions(pos);
+                            tmp.calculatePositions($scope.position);
+                            $scope.calculatePositions(position);
                             swapNodes(tmp.content[0], $element[0]);
 
                             $scope.container.redraw();
