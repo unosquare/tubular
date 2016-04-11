58bb8ae0ec64ac86d7079073b1fa062ed6514fa4
Geo Perez <geovanni.perez@gmail.com>
2016-04-11 14:21:52 -0500

    Update Chosen lib

--- Unosquare.Tubular.Sample/ui/chosen/tb-chosen.js
+++ Unosquare.Tubular.Sample/ui/chosen/tb-chosen.js
@@ -1,10 +1,11 @@
 ﻿(function() {
     "use strict";
-    
+
     var chosenCtrl = [
-        '$scope', 'tubularEditorService', function($scope, tubularEditorService) {
+        '$scope', 'tubularEditorService', '$timeout', '$element', function($scope, tubularEditorService, $timeout, $element) {
             tubularEditorService.setupScope($scope);
             $scope.dataIsLoaded = false;
+            $scope.alreadyChosen = false;
             $scope.selectOptions = "d for d in options";
 
             if (angular.isDefined($scope.optionLabel)) {
@@ -15,19 +16,22 @@
                 }
             }
 
-            $scope.$watch('value', function(val) {
-                $scope.$emit('tbForm_OnFieldChange', $scope.$component, $scope.name, val);
-
+            $scope.updateMetaData = function(val) {
                 if (angular.isDefined($scope.$component.model) && angular.isDefined($scope.options)) {
                     if (angular.isUndefined($scope.$component.model.$metadata))
                         $scope.$component.model.$metadata = {};
 
                     if (angular.isDefined($scope.options.filter))
-                        $scope.$component.model.$metadata[$scope.name] = $scope.options.filter(function (el) { return el[$scope.optionKey] == val });
+                        $scope.$component.model.$metadata[$scope.name] = $scope.options.filter(function(el) { return el[$scope.optionKey] == val });
                 }
+            }
+
+            $scope.$watch('value', function(val) {
+                $scope.$emit('tbForm_OnFieldChange', $scope.$component, $scope.name, val);
+                $scope.updateMetaData(val);
             });
 
-            $scope.resetEditor = function () {
+            $scope.resetEditor = function() {
                 $scope.dataIsLoaded = false;
                 $scope.value = null;
                 $scope.options = [];
@@ -45,7 +49,7 @@
                 }
 
                 var separator = $scope.optionsUrl.indexOf('?') === -1 ? '?' : '&';
-                
+
                 var currentRequest = $scope.$component.dataService.retrieveDataAsync({
                     serverUrl: $scope.optionsUrl + separator + '_=' + new Date().getTime(),
                     requestMethod: $scope.optionsMethod || 'GET'
@@ -55,7 +59,7 @@
                 $scope.value = $scope.multiple ? [] : '';
 
                 currentRequest.promise.then(
-                    function (data) {
+                    function(data) {
                         $scope.options = data;
 
                         if ($scope.allowEmpty) {
@@ -82,12 +86,27 @@
                                 var possibleValue = $scope.options && $scope.options.length > 0 ?
                                     angular.isDefined($scope.optionKey) ? $scope.options[0][$scope.optionKey] : $scope.options[0]
                                     : '';
+
+                                if (angular.isDefined(value) && value != null && $scope.options && $scope.options.length > 0 && angular.isDefined($scope.optionKey)) {
+                                    value = undefined;
+                                }
+
                                 $scope.value = value || $scope.defaultValue || possibleValue;
+                                $scope.updateMetaData($scope.value);
                             }
                         }
                     }, function(error) {
                         $scope.$emit('tbGrid_OnConnectionError', error);
-                    });
+                    }).then(function() {
+                    $timeout(function() {
+                        if ($scope.alreadyChosen) {
+                            $element.find('select').trigger("chosen:updated");
+                        } else {
+                            $element.find('select').chosen();
+                            $scope.alreadyChosen = true;
+                        }
+                    }, 1000);
+                });
             };
 
             if (angular.isDefined($scope.optionsUrl)) {
@@ -105,18 +124,23 @@
                         }
                     });
                 }
+            } else {
+                $timeout(function() {
+                    $element.find('select').chosen();
+                    $scope.alreadyChosen = true;
+                }, 1500);
             }
         }
     ];
 
-    angular.module('tubular.chosen', ['tubular.services', 'tubular.models', 'localytics.directives']).directive('tbChosen', [
+    angular.module('tubular.chosen', ['tubular.services', 'tubular.models']).directive('tbChosen', [
         'tubularEditorService', function(tubularEditorService) {
 
             return {
                 template: '<div ng-class="{ \'form-group\' : showLabel && isEditing, \'has-error\' : !$valid }">' +
                     '<span ng-hide="isEditing">{{ value }}</span>' +
-                    '<label ng-show="showLabel">{{ label }}</label>' +
-                    '<select chosen ng-options="{{ selectOptions }}" ng-show="isEditing" ng-model="value" class="form-control" ' +
+                    '<label ng-show="showLabel">{{ label }} <a href="{{helpLink}}" ng-show="helpLink" class="small">{{helpText}}</a></label>' +
+                    '<select ng-options="{{ selectOptions }}" ng-show="isEditing" ng-model="value" class="form-control" ' +
                     'ng-required="required" ng-disabled="readOnly" />' +
                     '<span class="help-block error-block" ng-show="isEditing" ng-repeat="error in state.$errors">' +
                     '{{error}}' +
@@ -126,7 +150,17 @@
                 restrict: 'E',
                 replace: true,
                 transclude: true,
-                scope: angular.extend({ options: '=?', optionsUrl: '@', optionsMethod: '@?', optionLabel: '@?', optionKey: '@?', defaultValue: '@?', allowEmpty: '=?' }, tubularEditorService.defaultScope),
+                scope: angular.extend({
+                    options: '=?',
+                    optionsUrl: '@',
+                    optionsMethod: '@?',
+                    optionLabel: '@?',
+                    optionKey: '@?',
+                    defaultValue: '@?',
+                    allowEmpty: '=?',
+                    helpLink: '@?',
+                    helpText: '@?'
+                }, tubularEditorService.defaultScope),
                 controller: chosenCtrl
             };
         }
@@ -137,7 +171,7 @@
                 template: '<div ng-class="{ \'form-group\' : showLabel && isEditing, \'has-error\' : !$valid }">' +
                     '<span ng-hide="isEditing">{{ value }}</span>' +
                     '<label ng-show="showLabel">{{ label }}</label>' +
-                    '<select chosen multiple ng-options="{{ selectOptions }}" ng-show="isEditing" ng-model="value" class="form-control" ' +
+                    '<select multiple ng-options="{{ selectOptions }}" ng-show="isEditing" ng-model="value" class="form-control" ' +
                     'ng-required="required" ng-disabled="readOnly" />' +
                     '<span class="help-block error-block" ng-show="isEditing" ng-repeat="error in state.$errors">' +
                     '{{error}}' +
@@ -147,76 +181,16 @@
                 restrict: 'E',
                 replace: true,
                 transclude: true,
-                scope: angular.extend({ options: '=?', optionsUrl: '@', optionsMethod: '@?', optionLabel: '@?', optionKey: '@?', defaultValue: '@?' }, tubularEditorService.defaultScope),
+                scope: angular.extend({
+                    options: '=?',
+                    optionsUrl: '@',
+                    optionsMethod: '@?',
+                    optionLabel: '@?',
+                    optionKey: '@?',
+                    defaultValue: '@?'
+                }, tubularEditorService.defaultScope),
                 controller: chosenCtrl
             };
         }
-    ]).directive('tbColumnChosenFilter', [
-            'tubularGridFilterService', function (tubularGridFilterService) {
-
-                return {
-                    require: '^tbColumn',
-                    template: '<div class="tubular-column-menu">' +
-                        '<button class="btn btn-xs btn-default btn-popover" ng-click="open()" ' +
-                        'ng-class="{ \'btn-success\': filter.HasFilter }">' +
-                        '<i class="fa fa-filter"></i></button>' +
-                        '<div style="display: none;">' +
-                        '<button type="button" class="close" data-dismiss="modal" ng-click="close()"><span aria-hidden="true">×</span></button>' +
-                        '<h4>{{::filterTitle}}</h4>' +
-                        '<form class="tubular-column-filter-form" onsubmit="return false;">' +
-                        '<select class="form-control" ng-model="filter.Text" ng-options="item for item in optionsItems" ' +
-                        ' ng-disabled="dataIsLoaded == false"></select>' +
-                        '<hr />' +
-                        '<tb-column-filter-buttons></tb-column-filter-buttons>' +
-                        '</form></div>' +
-                        '</div>',
-                    restrict: 'E',
-                    replace: true,
-                    transclude: true,
-                    scope: false,
-                    controller: [
-                        '$scope', '$timeout', function ($scope, $timeout) {
-                            $scope.getOptionsFromUrl = function () {
-                                $scope.$component.dataService.retrieveDataAsync({
-                                    serverUrl: $scope.filter.OptionsUrl,
-                                    requestMethod: 'GET'
-                                }).promise.then(function (data) {
-                                    $scope.optionsItems = data;
-                                    $scope.filter.Operator = 'Contains';
-
-                                    if (angular.isDefined($scope.optionLabel)) {
-                                        $scope.optionsItems = data.map(function (x) { return x[$scope.optionLabel]; });
-                                    }
-                                }, function (error) {
-                                    $scope.$emit('tbGrid_OnConnectionError', error);
-                                });
-                            };
-
-                            $scope.loadChosen = function (lElement) {
-                                $(lElement).find('.btn-popover').on('show.bs.popover', function (e) {
-                                    $timeout(function () {
-                                        $(lElement).find('div.popover').find('select').chosen();
-                                    }, 100);
-                                });
-                            };
-                        }
-                    ],
-                    compile: function compile() {
-                        return {
-                            pre: function (scope, lElement, lAttrs) {
-                                scope.optionLabel = lAttrs.optionLabel;
-                                scope.optionKey = lAttrs.optionKey;
-
-                                tubularGridFilterService.applyFilterFuncs(scope, lElement, lAttrs);
-                                tubularGridFilterService.createFilterModel(scope, lAttrs);
-                                scope.getOptionsFromUrl();
-                            },
-                            post: function (scope, lElement, lAttrs) {
-                                scope.loadChosen(lElement);
-                            }
-                        };
-                    }
-                };
-            }
     ]);
 })();
