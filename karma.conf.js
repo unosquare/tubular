ad138c94193e931d42084146d55af8f0fbbca8e6
Geo Perez <geovanni.perez@gmail.com>
2016-04-26 09:38:28 -0500

    Trying Karma

---
+++ karma.conf.js
@@ -0,0 +1,30 @@
+module.exports = function(config) {
+    config.set({
+        basePath: './',
+
+        files: [
+            'bower_components/jquery/dist/jquery.js',
+            'bower_components/angular/angular.js',
+            'bower_components/angular-route/angular-route.js',
+            'dist/tubular-bundle.js'
+        ],
+
+        autoWatch: true,
+
+        frameworks: ['jasmine'],
+
+        browsers: ['Chrome'],
+
+        plugins: [
+            'karma-chrome-launcher',
+            'karma-jasmine',
+            'karma-junit-reporter'
+        ],
+
+        junitReporter: {
+            outputFile: 'test_out/unit.xml',
+            suite: 'unit'
+        }
+
+    });
+};
