1c66fae5fd7c66e8d0da5aaf4975ee241c42e6a0
roberttapia <oscar.lopez@unosquare.com>
2016-05-02 14:30:25 -0500

    Updating protractor conf file

--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -9,7 +9,7 @@ exports.config = {
     'browserName': 'chrome'
   },
 
-  baseUrl: 'http://localhost:8000/tbTestApp/',
+  baseUrl: 'http://localhost:8000/Unosquare.Tubular.WebTest/',
 
   framework: 'jasmine',
 
