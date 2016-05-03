de6df4d9a7b237d911efac3439972758fdbc6c23
Geo Perez <geovanni.perez@gmail.com>
2016-05-03 16:56:50 -0500

    Add Jquery to protractor?

--- karma.conf.js
+++ karma.conf.js
@@ -3,6 +3,7 @@ module.exports = function(config) {
         basePath: './',
 
         files: [
+            'bower_components/jquery/dist/jquery.js',
             'bower_components/angular/angular.js',
             'bower_components/angular-route/angular-route.js',
             'bower_components/angular-mocks/angular-mocks.js',
