1e4a0922f056cde4fa1b87e3ef9e841ff91d10f5
Geo Perez <geovanni.perez@gmail.com>
2016-05-03 12:14:10 -0500

    Update Popover to use Angular UI

--- Unosquare.Tubular/Javascript/tubular/tubular-services-template.js
+++ Unosquare.Tubular/Javascript/tubular/tubular-services-template.js
@@ -18,6 +18,29 @@
                 me.enums = tubularTemplateServiceModule.enums;
                 me.defaults = tubularTemplateServiceModule.defaults;
 
+                // Loading popovers templates
+                me.tbColumnFilterPopoverTemplateName = 'tbColumnFilterPopoverTemplate.html';
+
+                if (!$templateCache.get(me.tbColumnFilterPopoverTemplateName)) {
+                    me.tbColumnFilterPopoverTemplate = '<div>' +
+                        '<form class="tubular-column-filter-form" onsubmit="return false;">' +
+                        '<select class="form-control" ng-options="key as value for (key , value) in $ctrl.filterOperators" ng-model="$ctrl.filter.Operator" ng-hide="$ctrl.dataType == \'boolean\'"></select>&nbsp;' +
+                        '<input class="form-control" type="search" ng-model="$ctrl.filter.Text" autofocus ng-keypress="$ctrl.checkEvent($event)" ng-hide="$ctrl.dataType == \'boolean\'"' +
+                        'placeholder="{{\'CAPTION_VALUE\' | translate}}" ng-disabled="$ctrl.filter.Operator == \'None\'" />' +
+                        '<div class="text-center" ng-show="$ctrl.dataType == \'boolean\'">' +
+                        '<button type="button" class="btn btn-default btn-md" ng-disabled="$ctrl.filter.Text === true" ng-click="$ctrl.filter.Text = true; $ctrl.filter.Operator = \'Equals\';">' +
+                        '<i class="fa fa-check"></i></button>&nbsp;' +
+                        '<button type="button" class="btn btn-default btn-md" ng-disabled="$ctrl.filter.Text === false" ng-click="$ctrl.filter.Text = false; $ctrl.filter.Operator = \'Equals\';">' +
+                        '<i class="fa fa-times"></i></button></div>' +
+                        '<input type="search" class="form-control" ng-model="$ctrl.filter.Argument[0]" ng-keypress="$ctrl.checkEvent($event)" ng-show="$ctrl.filter.Operator == \'Between\'" />' +
+                        '<hr />' +
+                        '<tb-column-filter-buttons></tb-column-filter-buttons>' +
+                        '</form></div>';
+
+                    $templateCache.put(me.tbColumnFilterPopoverTemplateName, me.tbColumnFilterPopoverTemplate);
+                }
+
+
                 me.generatePopup = function(model, title) {
                     var templateName = 'temp' + (new Date().getTime()) + '.html';
                     var template = tubularTemplateServiceModule.generatePopup(model, title);
