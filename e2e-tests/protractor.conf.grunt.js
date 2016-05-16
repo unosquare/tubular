b12b6b0ed56d1b97c22308d1b52c81fec0e9fe20
roberttapia <oscar.lopez@unosquare.com>
2016-05-16 15:32:57 -0500

    Revert "Revert "Cleaning project and adding e2e coverage info code""
    This reverts commit 1b9fbf7cbed042097a19b11e08417d6a79cda1fc.

--- e2e-tests/protractor.conf.grunt.js
+++ e2e-tests/protractor.conf.grunt.js
@@ -2,21 +2,12 @@ exports.config = {
     allScriptsTimeout: 11000,
 
     specs: [
-        'tbGridPager-scen.js',
-        'tbPageSizeSelector-scen.js',
-        'tbGridPagerInfo-scen.js',
-        'tbColumn-scen.js',
-        'tbFilters-scen.js',
-        'tbForm-scen.js',
-        'tbFormSave-scen.js',
-        'tbTemplateServiceModelSpec.js'
+        'e2e-tests/*.js'
     ],
 
     capabilities: {
         'browserName': 'chrome'
     },
-
-    baseUrl: 'http://localhost:8000/Unosquare.Tubular.WebTest/',
 
     framework: 'jasmine',
 
