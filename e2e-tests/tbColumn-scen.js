59bc71837e22d6a0622e11230f505356c9c31dd6
Geo Perez <geovanni.perez@gmail.com>
2016-06-27 15:44:20 -0500

    Adding a new Unit Test

--- e2e-tests/tbColumn-scen.js
+++ e2e-tests/tbColumn-scen.js
@@ -4,7 +4,7 @@
 // component at the related HTML file (tbColumn_tests.html) is static and constrained
 // to 53 records with consecutive ID's.
 
-describe('tbColumn', function () {
+describe('tbColumn', function() {
 
     var firstDataRow,
         lastDataRow,
@@ -14,21 +14,21 @@ describe('tbColumn', function () {
         a_shippedDateSorting,
         a_shipperCitySorting;
 
-    beforeAll(function () {
+    beforeAll(function() {
         // Go to test
         browser.get('index.html');
         element(by.id('testsSelector')).click();
         element(by.id('tbColumnSortingTest')).click();
-        
+
         // Go to first page if not there    
         element(by.tagName('tb-grid-pager'))
             .$('li.pagination-first a')
             .click();
-        
+
         // Select '100' on tbPageSizeSelector
         element(by.model('$ctrl.$component.pageSize'))
             .$('[value="number:100"]').click();
-            
+
         /**********************/
         // * Test variables * //
         /**********************/
@@ -54,7 +54,7 @@ describe('tbColumn', function () {
             .$('a');
     });
 
-    describe('sorting', function () {
+    describe('Grid Sorting', function() {
 
         var dataSetLowerID = '1',
             dataSetHigherID = '53',
@@ -62,65 +62,72 @@ describe('tbColumn', function () {
             dataSetHigherCustomerName = 'Vesta',
             dataSetLowerDate = /1\/28\/16 \d:17.*/,
             dataSetHigherDate = /5\/11\/16 \d:00.*/;
-        
-        beforeEach(function(){
+
+        beforeEach(function() {
             // Clear possible sortings and start with default
             a_orderIdSorting.click();
-            i_sortIcon.getAttribute('class').then(function (sortIconClass) {
+            i_sortIcon.getAttribute('class').then(function(sortIconClass) {
                 if (sortIconClass.indexOf('arrow') != -1) {
                     if (sortIconClass.indexOf('arrow-up') != -1) {
                         a_orderIdSorting.click();
                     }
                     a_orderIdSorting.click();
                 }
-            }); 
+            });
         });
 
-        it('should order data in ascending order when click-sorting an unsorted numeric column', function () {
+        it('should order data in ascending order when click-sorting an unsorted numeric column', function() {
             a_orderIdSorting.click();
-            
+
             expect(firstDataRow.$$('td').first().getText()).toBe(dataSetLowerID);
             expect(lastDataRow.$$('td').first().getText()).toBe(dataSetHigherID);
         });
-        
-        it('should order data in descending order when click-sorting an ascending-sorted numeric column', function(){
+
+        it('should order data in descending order when click-sorting an ascending-sorted numeric column', function() {
             a_orderIdSorting.click();
             a_orderIdSorting.click();
-                                    
+
             expect(firstDataRow.$$('td').first().getText()).toBe(dataSetHigherID);
             expect(lastDataRow.$$('td').first().getText()).toBe(dataSetLowerID);
         });
-        
-        it('should order data in ascending order when click-sorting an unsorted text column', function(){
+
+        it('should order data in ascending order when click-sorting an unsorted text column', function() {
             a_customerNameSorting.click();
-            
+
             expect(firstDataRow.$$('td').get(1).getText()).toBe(dataSetLowerCustomerName);
             expect(lastDataRow.$$('td').get(1).getText()).toBe(dataSetHigherCustomerName);
         });
-        
-        it('should order data in descending order when click-sorting an ascending-sorted text column', function(){
+
+        it('should order data in descending order when click-sorting an ascending-sorted text column', function() {
             a_customerNameSorting.click();
             a_customerNameSorting.click();
-            
+
             expect(firstDataRow.$$('td').get(1).getText()).toBe(dataSetHigherCustomerName);
             expect(lastDataRow.$$('td').get(1).getText()).toBe(dataSetLowerCustomerName);
         });
-        
-        it('should order data in ascending order when click-sorting an unsorted date column', function(){                
+
+        it('should order data in ascending order when click-sorting an unsorted date column', function() {
             a_shippedDateSorting.click();
-            
+
             expect(firstDataRow.$$('td').get(2).getText()).toMatch(dataSetLowerDate);
-            expect(lastDataRow.$$('td').get(2).getText()).toMatch(dataSetHigherDate);            
+            expect(lastDataRow.$$('td').get(2).getText()).toMatch(dataSetHigherDate);
         });
-        
-        it('should order data in descending order when click-sorting an ascending-sorted date column', function(){                
+
+        it('should order data in descending order when click-sorting an ascending-sorted date column', function() {
             a_shippedDateSorting.click();
             a_shippedDateSorting.click();
-            
+
             expect(firstDataRow.$$('td').get(2).getText()).toMatch(dataSetHigherDate);
-            expect(lastDataRow.$$('td').get(2).getText()).toMatch(dataSetLowerDate);            
+            expect(lastDataRow.$$('td').get(2).getText()).toMatch(dataSetLowerDate);
         });
-       
+
     });
 
+    describe('Grid Components', function() {
+        it('should show column selector', function() {
+            element(by.tagName('tb-column-selector')).click();
+
+            expect(element(by.css('div.modal')).isDisplayed()).toBe(true);
+        });
+    });
 });
