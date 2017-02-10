dab5552e69a0d3c21a88eb1ba67f86b3de4b5244
Geo Perez <geovanni.perez@gmail.com>
2017-02-10 16:51:44 -0600

    Add gulp to generate basic reports

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,11 @@ var Dgeni = require('dgeni');
 var fs = require('fs');
 var path = require('path');
 
+function getDirectories (srcpath) {
+  return fs.readdirSync(srcpath)
+    .filter(file => fs.statSync(path.join(srcpath, file)).isDirectory())
+}
+
 gulp.task('dgeni', function() {
     try {
         var dgeni = new Dgeni([require('./docs/dgeni-conf')]);
@@ -26,4 +31,22 @@ gulp.task('dgeni', function() {
         console.log(x.stack);
         throw x;
     }
+});
+
+gulp.task('reports', function() {
+    var baseCss = '<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootswatch/latest/lumen/bootstrap.min.css" />';
+    var output = '';
+
+    getDirectories('reports').forEach(function(dir) {
+        output += "<li><a href='https://unosquare.github.io/tubular/reports/" + dir + "'>" + dir + "</a></li>";
+
+        var subOutput = '';
+        getDirectories('reports/' + dir).forEach(function(subDir) {
+            subOutput += "<li><a href='https://unosquare.github.io/tubular/reports/" + dir + "/" + subDir + "'>" + subDir + "</a></li>";
+        });
+
+        fs.writeFile('reports/' + dir + '/index.html', baseCss + "<h1>Travis Build #" + dir +" Reports</h1><ul>" + subOutput + "</ul>");
+    }, this);
+
+    fs.writeFile('reports/index.html', baseCss + "<h1>Travis Builds Reports</h1><ul>" + output + "</ul>");
 });
