db8006456fe145c3ea1c40ffa543aecd8fe6c5b9
Gilberto L. <glangarica@seamgen.com>
2017-02-14 13:38:38 -0600

    Replace SendKeys to Clear function (test passes)

--- test/e2e-tests/tbForm-scen.js
+++ test/e2e-tests/tbForm-scen.js
@@ -532,7 +532,7 @@ describe('tbForm related components', function () {
         it('should be invalidated when the date is not in the range of "min" and "max" attributes', function () {
             var errorPresent = false;
             var messageCount;
-            tbDateEditor_input.sendKeys("").then(function () {
+            tbDateEditor_input.clear().then(function () {
                 // input  an invalid < min date
                 tbDateEditor_input.sendKeys("02/20/2015").then(function () {
                     tbDateEditor_errorMessages.getText().then(function (errorsArray) {
@@ -551,7 +551,7 @@ describe('tbForm related components', function () {
                         })
                             .then(function () {
                                 tbDateEditor_label.click();
-                                tbDateEditor_input.sendKeys("").then(function () {
+                                tbDateEditor_input.clear().then(function () {
                                     tbDateEditor_input.sendKeys("05/08/2016").then(function () {
                                         // Expect min date error to have been removed
                                         expect(tbDateEditor_errorMessages.count()).toBeLessThan(messageCount);
@@ -560,7 +560,7 @@ describe('tbForm related components', function () {
                             });
                     })
                     .then(function () {
-                        tbDateEditor_input.sendKeys("").then(function () {
+                        tbDateEditor_input.clear().then(function () {
                             tbDateEditor_input.sendKeys("06/11/2016").then(function () {
                                 // Expect max chars error to be displayed
                                 expect(tbDateEditor_errorMessages.count()).toBe(messageCount);
