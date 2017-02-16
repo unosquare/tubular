2e7b2e79fa8e48ce9d3ffe04cfaa7512315f6601
Alejandro Ocampo <kadosh.ivan@gmail.com>
2017-02-16 12:13:13 -0600

    Fixed issue with refresh token logic. Bearer token wasn't being updated when required.

--- src/Unosquare.Tubular/Javascript/tubular-bundle.js
+++ src/Unosquare.Tubular/Javascript/tubular-bundle.js
@@ -4547,7 +4547,7 @@
      * @constructor
      * @returns {Object} A httpInterceptor
      * 
-     */   
+     */
     angular.module('tubular.services')
         .factory('tubularAuthInterceptor', ['$q', '$injector', function ($q, $injector) {
             var authRequestRunning = null;
@@ -4617,6 +4617,7 @@
                                     tubularHttp.handleSuccessCallback(null, r.data);
 
                                     if (tubularHttp.requireAuthentication && tubularHttp.isAuthenticated()) {
+                                        rejection.config.headers.Authorization = "Bearer " + tubularHttp.userData.bearerToken;
                                         $injector.get('$http')(rejection.config).then(function (resp) {
                                             deferred.resolve(resp);
                                         }, function () {
