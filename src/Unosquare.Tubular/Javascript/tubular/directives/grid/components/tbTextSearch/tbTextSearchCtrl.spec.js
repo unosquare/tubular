6075bfdfcbd78b55902b65b76d29af14b7c630db
Alexey <alexey.turlapov@unosquare.com>
2017-02-17 10:25:55 -0600

    Adding more unit tests for tbTextSearchCtrl

--- src/Unosquare.Tubular/Javascript/tubular/directives/grid/components/tbTextSearch/tbTextSearchCtrl.spec.js
+++ src/Unosquare.Tubular/Javascript/tubular/directives/grid/components/tbTextSearch/tbTextSearchCtrl.spec.js
@@ -15,22 +15,53 @@ describe('Module: tubular.directives', function () {
         beforeEach(inject(function (_$controller_, $rootScope) {
             scope = $rootScope.$new();
             $controller = _$controller_;
-
+            
         }));
 
         function create() {
             sut = $controller('tbTextSearchController', { '$scope': scope });
+            sut.$component = {
+                search: {}
+            }
         }
 
-        describe('constructor', function () {
+        describe('Method: $onInit', function () {
 
             beforeEach(create);
 
-            it('should set tubularDirective properly', function () {
-                
+            it('should set default minChars properly', function () {
+                sut.$onInit();
+
+                expect(sut.minChars).toBe(3);
             })
 
-          
+            it('should not modify minChars if specified', function () {
+                sut.minChars = 5;
+
+                sut.$onInit();
+
+                expect(sut.minChars).toBe(5);
+            })
+
+            it('should set lastSearch properly', function () {
+                sut.$component.search.Text = "sometext";
+
+                sut.$onInit();
+
+                expect(sut.lastSearch).toBe("sometext");
+            })
+
+
+        });
+
+
+        describe('Watch: search.Text', function () {
+
+            beforeEach(create);
+
+           
+
+
         })
        
 
