32a7e8ddfb1e24c88f8c92e879544eb0a1d2fc0f
Alexey <alexey.turlapov@unosquare.com>
2017-02-17 10:00:35 -0600

    implementing textsearch unit tests

--- src/Unosquare.Tubular/Javascript/tubular/directives/grid/components/tbTextSearch/tbTextSearch.js
+++ src/Unosquare.Tubular/Javascript/tubular/directives/grid/components/tbTextSearch/tbTextSearch.js
@@ -22,10 +22,10 @@
             '<div class="tubular-grid-search">' +
                 '<div class="input-group input-group-sm">' +
                 '<span class="input-group-addon"><i class="fa fa-search"></i></span>' +
-                '<input type="search" class="form-control" placeholder="{{:: $ctrl.placeholder || (\'UI_SEARCH\' | translate) }}" maxlength="20" ' +
-                'ng-model="$ctrl.$component.search.Text" ng-model-options="{ debounce: 300 }">' +
-                '<span class="input-group-btn" ng-show="$ctrl.$component.search.Text.length > 0">' +
-                '<button class="btn btn-default" uib-tooltip="{{\'CAPTION_CLEAR\' | translate}}" ng-click="$ctrl.$component.search.Text = \'\'">' +
+                '<input type="search" name="tbTextSearchInput" class="form-control" placeholder="{{:: $ctrl.placeholder || (\'UI_SEARCH\' | translate) }}" maxlength="20" ' +
+                'ng-model="$ctrl.$component.search.Text" >' +
+                '<span id="tb-text-search-reset-panel" class="input-group-btn" ng-show="$ctrl.$component.search.Text.length > 0">' +
+                '<button id="tb-text-search-reset-button" class="btn btn-default" uib-tooltip="{{\'CAPTION_CLEAR\' | translate}}" ng-click="$ctrl.$component.search.Text = \'\'">' +
                 '<i class="fa fa-times-circle"></i>' +
                 '</button>' +
                 '</span>' +
--- src/Unosquare.Tubular/Javascript/tubular/directives/grid/components/tbTextSearch/tbTextSearch.spec.js
+++ src/Unosquare.Tubular/Javascript/tubular/directives/grid/components/tbTextSearch/tbTextSearch.spec.js
@@ -2,10 +2,12 @@
 
 describe('Module: tubular.directives', function () {
     describe('component: tbTextSearch', function () {
-        var sut, ctrl, scope, $compile, isolated, element, gridCtrl, filter;
+        var sut, ctrl, scope, $compile, isolated, element, gridCtrl, filter, timeout;
 
         beforeEach(function () {
             module('tubular.directives');
+
+            
             module(function ($controllerProvider, $filterProvider) {
                 
                 filter = jasmine.createSpy().and.returnValue('translated');
@@ -14,22 +16,22 @@ describe('Module: tubular.directives', function () {
                 $controllerProvider.register('tbTextSearchController', function ($scope) { });
 
             })
-
+           
 
         });
 
-        beforeEach(inject(function (_$compile_, _$rootScope_) {
+        beforeEach(inject(function (_$compile_, _$rootScope_, _$timeout_) {
             scope = _$rootScope_.$new();
             $compile = _$compile_;
-
+            timeout = _$timeout_;
         }));
 
         function generate(tpl) {
             element = angular.element(tpl);
             element.data('$tbGridController', gridCtrl);
-            $compile(element)(scope)
+            element = $compile(element)(scope)
             
-            scope.$apply();
+            scope.$digest();
             ctrl = element.controller('tbTextSearch');
         }
 
@@ -37,6 +39,7 @@ describe('Module: tubular.directives', function () {
             generate('<tb-text-search placeholder="search me" ></tb-text-search>')
 
             expect(ctrl.placeholder).toBe('search me');
+            expect(filter).not.toHaveBeenCalledWith('UI_SEARCH');
 
         })
 
@@ -51,6 +54,65 @@ describe('Module: tubular.directives', function () {
             generate('<tb-text-search min-chars="6" placeholder="search me"></tb-text-search>')
 
             expect(ctrl.minChars).toBe('6');
+
+        })
+
+        it('should translate the input placeholder', function () {
+            generate('<tb-text-search ></tb-text-search>')
+
+            expect(filter).toHaveBeenCalledWith('UI_SEARCH');
+
+        })
+
+        it('should translate the input placeholder', function () {
+            generate('<tb-text-search ></tb-text-search>')
+
+            expect(filter).toHaveBeenCalledWith('CAPTION_CLEAR');
+
+        })
+
+        it('input should set the parent search text', function () {
+            generate('<form name="form1"><tb-text-search ></tb-text-search></form>')
+            
+            gridCtrl.search.Text = '';
+            timeout(function () {
+                scope.form1.tbTextSearchInput.$setViewValue('google');
+            }, 350)
+            
+            scope.$digest();
+            timeout.flush();
+            expect(gridCtrl.search.Text).toBe('google');
+            
+            
+
+        })
+
+       
+
+        it('reset button should be visible only when the input has text', function () {
+            generate('<tb-text-search ></tb-text-search>')
+
+            gridCtrl.search.Text = "search me";
+            scope.$apply();
+            var panel = angular.element(element[0].querySelector('#tb-text-search-reset-panel'));
+            expect(panel.hasClass('ng-hide')).toBe(false);
+            gridCtrl.search.Text = "";
+            scope.$apply();
+            expect(panel.hasClass('ng-hide')).toBe(true);
+            
+            
+
+        })
+
+        it('reset button should reset the search input', function () {
+            generate('<tb-text-search ></tb-text-search>')
+
+            gridCtrl.search.Text = "search me";
+            element.find('button')[0].click();
+            scope.$apply();
+            expect(gridCtrl.search.Text).toBe('');
+
+
 
         })
     })
