bf40a190480c9405861897880e135e4399e7acf6
Geo Perez <geovanni.perez@gmail.com>
2017-02-22 15:55:21 -0600

    Adding test to tubularHttp

--- src/Unosquare.Tubular/Javascript/tubular/services/http.spec.js
+++ src/Unosquare.Tubular/Javascript/tubular/services/http.spec.js
@@ -14,12 +14,43 @@ describe('Module: tubular.services', function () {
 
             inject(function ($injector, _tubularHttp_) {
                 $httpBackend = $injector.get('$httpBackend');
+
+                $httpBackend.whenRoute('GET', '/api/get')
+                    .respond(function () { return [200, { result: true }]; });
                 tubularHttp = _tubularHttp_;
             });
         });
 
         it('should be defined', function () {
             expect(tubularHttp).toBeDefined();
+        });
+
+        it('should has default values', function () {
+            expect(tubularHttp.useRefreshTokens).toBe(false);
+            expect(tubularHttp.refreshTokenUrl).toBe('/api/token');
+            expect(tubularHttp.tokenUrl).toBe('/api/token');
+            expect(tubularHttp.apiBaseUrl).toBe('/api');
+        });
+
+        it('should set require authentication', function() {
+            tubularHttp.setRequireAuthentication(true);
+            expect(tubularHttp.requireAuthentication).toBe(true);
+            tubularHttp.setRequireAuthentication(false);
+            expect(tubularHttp.requireAuthentication).toBe(false);
+        });
+
+        it('should retrieve a promise when GET', function() {
+            var getPromise = tubularHttp.get('/api/get');
+            expect(getPromise).toBeDefined();
+            expect(getPromise.promise).toBeDefined();
+        });
+
+        it('should retrieve data when GET', function () {
+            tubularHttp.get('/api/get')
+                .promise.then(function(data) {
+                    expect(data).toBeDefined();
+                    expect(data.result).toBe(true);
+                });
         });
     });
 });
