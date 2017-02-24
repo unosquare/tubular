6430b31b4b598386ee40c30e85ea2a2249b68e33
Geo Perez <geovanni.perez@gmail.com>
2017-02-24 10:59:58 -0600

    Update Documentation

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,9 @@
-(function(angular) {
+(function (angular) {
     'use strict';
 
     angular.module('app.routes', ['ngRoute'])
         .config([
-            '$routeProvider', function($routeProvider) {
+            '$routeProvider', function ($routeProvider) {
                 $routeProvider.
                     when('/', {
                         templateUrl: 'assets/intro.html',
@@ -33,110 +33,106 @@
 
     angular.module('app.controllers', ['tubular.services', 'LocalStorageModule', 'app.generator'])
         .controller('tubularSampleCtrl', [
-            '$scope', '$location', '$anchorScroll', '$templateCache', '$http', 'tubularGenerator',
-            function($scope, $location, $anchorScroll, $templateCache, $http, tubularGenerator) {
-                $scope.source = [];
+            '$scope', '$location', '$anchorScroll', '$templateCache', '$http', 'tubularGenerator', 'toastr',
+    function ($scope, $location, $anchorScroll, $templateCache, $http, tubularGenerator, toastr) {
+        $scope.source = [];
 
-                $scope.tutorial = [
-                    {
-                        title: 'Basic Layout with JSON data source',
-                        body: 'First grid shows a basic layout, without any additional feature or special column. ' +
-                            'Just a plain grid using a JSON data source.',
-                        key: 'sample',
-                        next: 'samplesharp1'
-                    },
-                    {
-                        title: 'Grid with Paginations',
-                        body: 'Adding a new feature: the pagination. You can move across the pages and change the size.',
-                        key: 'samplesharp1',
-                        next: 'sample3'
-                    },
-                    {
-                        title: 'Grid with common features',
-                        body: 'The grid can be extended to include features like sorting and filtering. ' +
-                            'Press Ctrl key to sort by multiple columns.',
-                        key: 'sample3',
-                        next: 'sample4'
-                    },
-                    {
-                        title: 'Free-text search',
-                        body: 'Adding a "searchable" attribute to your columns and you can perform free-text searches.',
-                        key: 'sample4',
-                        next: 'sample5'
-                    },
-                    {
-                        title: 'Print and Export to CSV',
-                        body: 'Easily you can print or export the current view or entire dataset to CSV using client-side only.',
-                        key: 'sample5',
-                        next: 'sample7'
-                    },
-                    {
-                        title: 'Inline editors (read-only)',
-                        body: 'You can add inline editors just defining a Save URL and assigning some controls. ' +
-                            'This demo is read-only, but you can get the idea.',
-                        key: 'sample7',
-                        next: null
-                    },
-                    {
-                        title: 'Grid with Aggregation results',
-                        body: 'This sample has more controls in the grid and also two columns has aggregation methods. Customer count and Amount sum. The tbFootSet directive is helpful to show them.',
-                        key: 'sample8',
-                        next: null
-                    }
-                ];
+        $scope.tutorial = [
 
-                $scope.chartTutorial = [
-                    {
-                        title: 'Basic line chart with ChartJS',
-                        body: 'Easily add a chart using a REST Service with the Tubular C# connector. In this sample we are using a JSON file, because there is not support to server side in this documentation',
-                        key: 'samplechart1',
-                        next: 'samplechart2'
-                    }, {
-                        title: 'Basic line chart with Highcharts',
-                        body: 'Easily add a chart using a REST Service with the Tubular C# connector. In this sample we are using a JSON file, because there is not support to server side in this documentation',
-                        key: 'samplechart2',
-                        next: null
-                    }
-                ];
-
-                $scope.$on('tbGrid_OnSuccessfulUpdate', function(data) { toastr.success("Record updated"); });
-                $scope.$on('tbGrid_OnRemove', function(data) { toastr.success("Record removed"); });
-                $scope.$on('tbGrid_OnConnectionError', function(error) { toastr.error(error.statusText || "Connection error"); });
-                $scope.$on('tbGrid_OnSuccessfulForm', function(data) { $location.path('/'); });
-                $scope.$on('tbGrid_OnSavingNoChanges', function(model) {
-                    toastr.warning("Nothing to save");
-                    $location.path('/');
-                });
-
-                $scope.toggleCode = function(tag) {
-                    if ($scope.source[tag] == null) {
-                        $scope.source[tag] = $templateCache.get('assets/' + tag + '.html');
-                    } else {
-                        $scope.source[tag] = null;
-                    }
-                };
-
-                $scope.scrollTo = function(id) {
-                    $location.hash(id);
-                    $anchorScroll();
-                };
-
-                $scope.openCode = function(tag) {
-                    $http.get('generator/index.html').
-                        success(function(data) {
-                            tubularGenerator.exportPluker([
-                                { name: 'index.html', content: data },
-                                { name: 'README.md', content: tubularGenerator.DefaultReadme },
-                                { name: 'app.js', content: tubularGenerator.DefaultJs },
-                                { name: 'grid.html', content: $templateCache.get('assets/' + tag + '.html') }
-                            ]);
-                        });
-                }
+            {
+                title: 'Grid with Paginations',
+                body: 'Adding a new feature: the pagination. You can move across the pages and change the size.',
+                key: 'samplesharp1',
+                next: 'sample3'
+            },
+            {
+                title: 'Grid with common features',
+                body: 'The grid can be extended to include features like sorting and filtering. ' +
+                    'Press Ctrl key to sort by multiple columns.',
+                key: 'sample3',
+                next: 'sample4'
+            },
+            {
+                title: 'Free-text search',
+                body: 'Adding a "searchable" attribute to your columns and you can perform free-text searches.',
+                key: 'sample4',
+                next: 'sample5'
+            },
+            {
+                title: 'Print and Export to CSV',
+                body: 'Easily you can print or export the current view or entire dataset to CSV using client-side only.',
+                key: 'sample5',
+                next: 'sample7'
+            },
+            {
+                title: 'Inline editors (read-only)',
+                body: 'You can add inline editors just defining a Save URL and assigning some controls. ' +
+                    'This demo is read-only, but you can get the idea.',
+                key: 'sample7',
+                next: null
+            },
+            {
+                title: 'Grid with Aggregation results',
+                body: 'This sample has more controls in the grid and also two columns has aggregation methods. Customer count and Amount sum. The tbFootSet directive is helpful to show them.',
+                key: 'sample8',
+                next: null
             }
+        ];
+
+        $scope.chartTutorial = [
+            {
+                title: 'Basic line chart with ChartJS',
+                body: 'Easily add a chart using a REST Service with the Tubular C# connector. In this sample we are using a JSON file, because there is not support to server side in this documentation',
+                key: 'samplechart1',
+                next: 'samplechart2'
+            }, {
+                title: 'Basic line chart with HighCharts',
+                body: 'Easily add a chart using a REST Service with the Tubular C# connector. In this sample we are using a JSON file, because there is not support to server side in this documentation',
+                key: 'samplechart2',
+                next: null
+            }
+        ];
+
+        $scope.$on('tbGrid_OnSuccessfulUpdate', function () { toastr.success("Record updated"); });
+        $scope.$on('tbGrid_OnRemove', function () { toastr.success("Record removed"); });
+        $scope.$on('tbGrid_OnConnectionError', function(error) {
+            toastr.error(error.statusText || "Connection error");
+        });
+        $scope.$on('tbGrid_OnSuccessfulForm', function () { $location.path('/'); });
+        $scope.$on('tbGrid_OnSavingNoChanges', function () {
+            toastr.warning("Nothing to save");
+            $location.path('/');
+        });
+
+        $scope.toggleCode = function (tag) {
+            if ($scope.source[tag] == null) {
+                $scope.source[tag] = $templateCache.get('assets/' + tag + '.html');
+            } else {
+                $scope.source[tag] = null;
+            }
+        };
+
+        $scope.scrollTo = function (id) {
+            $location.hash(id);
+            $anchorScroll();
+        };
+
+        $scope.openCode = function (tag) {
+            $http.get('generator/index.html').
+                success(function (data) {
+                    tubularGenerator.exportPluker([
+                        { name: 'index.html', content: data },
+                        { name: 'README.md', content: tubularGenerator.DefaultReadme },
+                        { name: 'app.js', content: tubularGenerator.DefaultJs },
+                        { name: 'grid.html', content: $templateCache.get('assets/' + tag + '.html') }
+                    ]);
+                });
+        }
+    }
         ])
         .controller('tubularGeneratorCtrl', [
             '$scope', '$http', '$templateCache', 'tubularGenerator', 'localStorageService', 'tubularTemplateService',
-            function($scope, $http, $templateCache, tubularGenerator, localStorageService, tubularTemplateService) {
+            function ($scope, $http, $templateCache, tubularGenerator, localStorageService, tubularTemplateService) {
                 // Options
                 $scope.enums = tubularTemplateService.enums;
 
@@ -153,14 +149,14 @@
                 $scope.views = localStorageService.get('generator_views') || [];
                 $scope.gridId = ($scope.views.length + 1);
 
-                $scope.generateModel = function() {
+                $scope.generateModel = function () {
                     $scope.templatename = '';
                     $scope.jsonstring = '';
 
                     if ($scope.basemodel.indexOf('http') === 0) {
                         $scope.dataUrl = $scope.basemodel;
 
-                        $http.get($scope.basemodel).success(function(data) {
+                        $http.get($scope.basemodel).success(function (data) {
                             tubularGenerator.createColumns(data.value, $scope);
                             $scope.uiOptions.ServiceName = 'odata';
                             $scope.formOptions.ServiceName = 'odata';
@@ -178,7 +174,7 @@
                     }
                 };
 
-                $scope.runFormTemplate = function() {
+                $scope.runFormTemplate = function () {
                     $scope.gridmodel = tubularGenerator.runFormTemplate($scope);
 
                     $templateCache.put('tubulartemplate.html', $scope.gridmodel);
@@ -187,7 +183,7 @@
                     $scope.step++;
                 }
 
-                $scope.runGridTemplate = function() {
+                $scope.runGridTemplate = function () {
                     $scope.gridmodel = tubularGenerator.runGridTemplate($scope);
 
                     $templateCache.put('tubulartemplate.html', $scope.gridmodel);
@@ -196,32 +192,32 @@
                     $scope.step++;
                 };
 
-                $scope.useSample = function() {
+                $scope.useSample = function () {
                     $http.get('data/generatorsample.json').
-                        success(function(data) {
+                        success(function (data) {
                             $scope.basemodel = angular.toJson(data);
                             $scope.generateModel();
                         });
                 };
 
-                $scope.useServerSample = function() {
+                $scope.useServerSample = function () {
                     $scope.basemodel = "http://services.odata.org/V3/Northwind/Northwind.svc/Orders";
                     $scope.generateModel();
                 };
 
-                $scope.revert = function() {
+                $scope.revert = function () {
                     $scope.templatename = '';
                     $scope.step--;
                 };
 
-                $scope.cleanPlunker = function(filename) {
+                $scope.cleanPlunker = function (filename) {
                     $scope.clearViews();
                     $scope.plunker(filename);
                 }
 
-                $scope.plunker = function(filename) {
+                $scope.plunker = function (filename) {
                     $http.get('generator/index.html').
-                        success(function(data) {
+                        success(function (data) {
                             var appJs = tubularGenerator.DefaultJs;
                             var files = [
                                 { name: 'index.html', content: data },
@@ -260,7 +256,7 @@
                         });
                 };
 
-                $scope.save = function(filename) {
+                $scope.save = function (filename) {
                     var tempUrl = $scope.dataUrl;
 
                     if (angular.isDefined($scope.jsonstring) && $scope.jsonstring !== '') {
@@ -278,13 +274,13 @@
                     $scope.basemodel = '';
                 };
 
-                $scope.clearViews = function() {
+                $scope.clearViews = function () {
                     localStorageService.remove('generator_views');
                     $scope.views = [];
                     $scope.gridId = ($scope.views.length + 1);
                 }
 
-                $scope.removeColumn = function(row) {
+                $scope.removeColumn = function (row) {
                     var index = $scope.columns.indexOf(row);
                     if (index > -1) {
                         $scope.columns.splice(index, 1);
@@ -295,30 +291,30 @@
         .controller('tubularDocCtrl',
         [
             '$scope', '$http', '$anchorScroll', '$location', '$routeParams', '$templateCache',
-            function($scope, $http, $anchorScroll, $location, $routeParams, $templateCache) {
-                $scope.internalLink = function(url) {
-                    var find = $scope.items.filter(function(el) { return el.name === url; });
+            function ($scope, $http, $anchorScroll, $location, $routeParams, $templateCache) {
+                $scope.internalLink = function (url) {
+                    var find = $scope.items.filter(function (el) { return el.name === url; });
 
                     if (find.length > 0) {
                         $scope.open(find[0].url);
                     }
                 };
 
-                $scope.getHtml = function(url) {
+                $scope.getHtml = function (url) {
                     var data = $templateCache.get(url);
                     return data;
                 };
 
-                $scope.open = function(url) {
-                    if (url.indexOf('http') == 0) {
+                $scope.open = function (url) {
+                    if (url.indexOf('http') === 0) {
                         document.location = url;
                         return;
                     }
 
-                    $scope.doc = url.indexOf('tutorial') == 0 ? url : 'docs/build/' + url;
+                    $scope.doc = url.indexOf('tutorial') === 0 ? url : 'docs/build/' + url;
                 };
 
-                $http.get('data/documentation.json').then(function(data) {
+                $http.get('data/documentation.json').then(function (data) {
                     $scope.items = data.data;
 
                     // Load Angular Components API
@@ -349,21 +345,21 @@
                 });
             }
         ]).controller('infoCtrl', [
-            '$route', '$routeParams', '$location', function($route, $routeParams, $location) {
+            '$route', '$routeParams', '$location', function ($route, $routeParams, $location) {
                 this.$route = $route;
                 this.$location = $location;
                 this.$routeParams = $routeParams;
             }
         ])
         .config([
-            '$sceDelegateProvider', function($sceDelegateProvider) {
+            '$sceDelegateProvider', function ($sceDelegateProvider) {
                 $sceDelegateProvider.resourceUrlWhitelist(['self', 'blob:**', 'http://services.odata.org/**']);
             }
         ]);
 
     angular.module('app', [
         'hljs',
-        'angular.filter',
+        'toastr',
         'tubular',
         'tubular-chart.directives',
         'tubular-hchart.directives',
