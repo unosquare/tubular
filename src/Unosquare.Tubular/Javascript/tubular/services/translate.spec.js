e4bb9a832051cb69adefeee217279ee290fbbede
Gilberto L. <glangarica@seamgen.com>
2017-03-09 11:02:21 -0600

    Adding some  test cases to translate.spec

--- src/Unosquare.Tubular/Javascript/tubular/services/translate.spec.js
+++ src/Unosquare.Tubular/Javascript/tubular/services/translate.spec.js
@@ -16,5 +16,34 @@ describe('Module: tubular.services', function () {
         it('should be defined', function () {
             expect(tubularTranslate).toBeDefined();
         });
+
+        it('should have default and current language to be "en"', function () {
+            expect(tubularTranslate.currentLanguage && tubular.defaultLanguage).toBe('en');
+        });
+
+        it('should have translation tables to have same keys', function () {
+            tubularTranslate.translationTable.forEach(value, key);
+        });
+
+        it('should change the language', function () {
+            tubularTranslate.setLanguage('es');
+            expect(tubularTranslate.currentLanguage).toBe('es');
+
+        });
+
+        it('should translate', function () {
+            tubularTranslate.setLanguage('es');
+            var result = tubularTranslate.translate('OP_EQUALS');
+            expect(result).toBe('Equals');
+
+        });
+
+        it('should add new translation', function () {
+
+            tubularTranslate.addTranslation('it', 'OP_STARTSWITH', 'Inizia con');
+            expect(translationTable[2]).toBe('it');
+
+        });
+
     });
 });
