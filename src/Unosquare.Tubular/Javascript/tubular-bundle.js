9c772732e23ba7d2762697a35b96e35a76358aa7
Alejandro Ocampo <kadosh.ivan@gmail.com>
2017-03-09 12:41:48 -0600

    Added TubularConfig provider. Migration of several settings still pending.

--- src/Unosquare.Tubular/Javascript/tubular-bundle.js
+++ src/Unosquare.Tubular/Javascript/tubular-bundle.js
@@ -4836,3 +4836,126 @@ angular.module('tubular.directives').run(['$templateCache', function ($templateC
             }
         ]);
 })(angular);
+(function (angular) {
+    'use strict';
+
+    angular.module('tubular.services')
+        .provider('tubularConfig', function () {
+
+            var provider = this;
+            provider.platform = {};
+            var PLATFORM = 'platform';
+
+            var configProperties = {
+                webApi: {
+                    tokenUrl: PLATFORM,
+                    refreshTokenUrl: PLATFORM,
+                    enableRefreshTokens: PLATFORM,
+                    authenticateRequests: PLATFORM,
+                    baseUrl: PLATFORM
+                },
+                platform: {}
+            };
+
+            createConfig(configProperties, provider, '');
+
+            // Default
+            // -------------------------
+            setPlatformConfig('default', {
+                webApi: {
+                    tokenUrl: '/api/token',
+                    refreshTokenUrl: '/api/token',
+                    enableRefreshTokens: false,
+                    authenticateRequests: true,
+                    baseUrl: '/api'
+                }
+            });
+
+            // private: used to set platform configs
+            function setPlatformConfig(platformName, platformConfigs) {
+                configProperties.platform[platformName] = platformConfigs;
+                provider.platform[platformName] = {};
+
+                addConfig(configProperties, configProperties.platform[platformName]);
+
+                createConfig(configProperties.platform[platformName], provider.platform[platformName], '');
+            }
+
+
+            // private: used to recursively add new platform configs
+            function addConfig(configObj, platformObj) {
+                for (var n in configObj) {
+                    if (n != PLATFORM && configObj.hasOwnProperty(n)) {
+                        if (angular.isObject(configObj[n])) {
+                            if (!isDefined(platformObj[n])) {
+                                platformObj[n] = {};
+                            }
+                            addConfig(configObj[n], platformObj[n]);
+
+                        } else if (!isDefined(platformObj[n])) {
+                            platformObj[n] = null;
+                        }
+                    }
+                }
+            }
+
+
+            // private: create methods for each config to get/set
+            function createConfig(configObj, providerObj, platformPath) {
+                angular.forEach(configObj, function (value, namespace) {
+
+                    if (angular.isObject(configObj[namespace])) {
+                        // recursively drill down the config object so we can create a method for each one
+                        providerObj[namespace] = {};
+                        createConfig(configObj[namespace], providerObj[namespace], platformPath + '.' + namespace);
+
+                    } else {
+                        // create a method for the provider/config methods that will be exposed
+                        providerObj[namespace] = function (newValue) {
+                            if (arguments.length) {
+                                configObj[namespace] = newValue;
+                                return providerObj;
+                            }
+                            if (configObj[namespace] == PLATFORM) {
+                                // if the config is set to 'platform', then get this config's platform value
+                                // var platformConfig = stringObj(configProperties.platform, 'default' + platformPath + '.' + namespace);
+                                // if (platformConfig || platformConfig === false) {
+                                //     return platformConfig;
+                                // }
+                                // didnt find a specific platform config, now try the default
+                                return stringObj(configProperties.platform, 'default' + platformPath + '.' + namespace);
+                            }
+                            return configObj[namespace];
+                        };
+                    }
+
+                });
+            }
+
+            function stringObj(obj, str) {
+                str = str.split(".");
+                for (var i = 0; i < str.length; i++) {
+                    if (obj && angular.isDefined(obj[str[i]])) {
+                        obj = obj[str[i]];
+                    } else {
+                        return null;
+                    }
+                }
+                return obj;
+            }
+
+            provider.setPlatformConfig = setPlatformConfig;
+
+
+            // private: Service definition for internal Tubular use
+            /**
+             * @ngdoc service
+             * @name tubularConfig
+             * @module TubularModule
+             * @private
+             */
+            provider.$get = function () {
+                return provider;
+            };
+        });
+})(angular);
