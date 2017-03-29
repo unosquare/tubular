10f39a32cf2d79783a0451571e32956cfd081c71
greciaveronica <greciaveronica@gmail.com>
2017-03-29 16:22:26 -0600

    Moving popup controller to a isolate file

--- src/Unosquare.Tubular/Javascript/tubular-bundle.js
+++ src/Unosquare.Tubular/Javascript/tubular-bundle.js
@@ -3861,61 +3861,84 @@ angular.module('tubular.directives').run(['$templateCache', function ($templateC
                             backdropClass: 'fullHeight',
                             animation: false,
                             size: size,
-                            controller:
-                                // TODO: Move out of this scope
-                                [
-                                '$scope', function ($scope) {
-                                    $scope.Model = model;
-
-                                    $scope.savePopup = function (innerModel, forceUpdate) {
-                                        innerModel = innerModel || $scope.Model;
-
-                                        // If we have nothing to save and it's not a new record, just close
-                                        if (!forceUpdate && !innerModel.$isNew && !innerModel.$hasChanges) {
-                                            $scope.closePopup();
-                                            return null;
-                                        }
-
-                                        var result = innerModel.save(forceUpdate);
-
-                                        if (angular.isUndefined(result) || result === false) {
-                                            return null;
-                                        }
-
-                                        result.then(
-                                            function (data) {
-                                                $scope.$emit('tbForm_OnSuccessfulSave', data);
-                                                $rootScope.$broadcast('tbForm_OnSuccessfulSave', data);
-                                                $scope.Model.$isLoading = false;
-                                                if (gridScope.autoRefresh) gridScope.retrieveData();
-                                                dialog.close();
-
-                                                return data;
-                                            },
-                                            function (error) {
-                                                $scope.$emit('tbForm_OnConnectionError', error);
-                                                $rootScope.$broadcast('tbForm_OnConnectionError', error);
-                                                $scope.Model.$isLoading = false;
-
-                                                return error;
-                                            });
-
-                                        return result;
-                                    };
-
-                                    $scope.closePopup = function () {
-                                        if (angular.isDefined($scope.Model.revertChanges)) {
-                                            $scope.Model.revertChanges();
-                                        }
-
-                                        dialog.close();
-                                    };
+                            controller: 'popupCtrl',
+                            resolve: {
+                                model: function () {
+                                    return model;
+                                },
+                                gridScope: function () {
+                                    return gridScope;
                                 }
-                                ]
+                            }
                         });
 
                         return dialog;
                     }
+                };
+            }
+        ]);
+})(angular);
+(function (angular) {
+    'use strict';
+
+    angular.module('tubular.services')
+        .controller('popupCtrl', [
+            '$rootScope',
+            '$scope',
+            '$uibModalInstance',
+            'model',
+            'gridScope',
+            function (
+                $rootScope,
+                $scope,
+                $uibModalInstance,
+                model,
+                gridScope) {
+
+                $scope.Model = model;
+
+                $scope.savePopup = function (innerModel, forceUpdate) {
+                    innerModel = innerModel || $scope.Model;
+
+                    // If we have nothing to save and it's not a new record, just close
+                    if (!forceUpdate && !innerModel.$isNew && !innerModel.$hasChanges) {
+                        $scope.closePopup();
+                        return null;
+                    }
+
+                    var result = innerModel.save(forceUpdate);
+
+                    if (angular.isUndefined(result) || result === false) {
+                        return null;
+                    }
+
+                    result.then(
+                        function (data) {
+                            $scope.$emit('tbForm_OnSuccessfulSave', data);
+                            $rootScope.$broadcast('tbForm_OnSuccessfulSave', data);
+                            $scope.Model.$isLoading = false;
+                            if (gridScope.autoRefresh) gridScope.retrieveData();
+                            $uibModalInstance.close();
+
+                            return data;
+                        },
+                        function (error) {
+                            $scope.$emit('tbForm_OnConnectionError', error);
+                            $rootScope.$broadcast('tbForm_OnConnectionError', error);
+                            $scope.Model.$isLoading = false;
+
+                            return error;
+                        });
+
+                    return result;
+                };
+
+                $scope.closePopup = function () {
+                    if (angular.isDefined($scope.Model.revertChanges)) {
+                        $scope.Model.revertChanges();
+                    }
+
+                    $uibModalInstance.close();
                 };
             }
         ]);
---
+++ src/Unosquare.Tubular/Javascript/tubular/services/popupCtrl.js
@@ -0,0 +1,65 @@
+﻿(function (angular) {
+    'use strict';
+
+    angular.module('tubular.services')
+        .controller('popupCtrl', [
+            '$rootScope',
+            '$scope',
+            '$uibModalInstance',
+            'model',
+            'gridScope',
+            function (
+                $rootScope,
+                $scope,
+                $uibModalInstance,
+                model,
+                gridScope) {
+
+                $scope.Model = model;
+
+                $scope.savePopup = function (innerModel, forceUpdate) {
+                    innerModel = innerModel || $scope.Model;
+
+                    // If we have nothing to save and it's not a new record, just close
+                    if (!forceUpdate && !innerModel.$isNew && !innerModel.$hasChanges) {
+                        $scope.closePopup();
+                        return null;
+                    }
+
+                    var result = innerModel.save(forceUpdate);
+
+                    if (angular.isUndefined(result) || result === false) {
+                        return null;
+                    }
+
+                    result.then(
+                        function (data) {
+                            $scope.$emit('tbForm_OnSuccessfulSave', data);
+                            $rootScope.$broadcast('tbForm_OnSuccessfulSave', data);
+                            $scope.Model.$isLoading = false;
+                            if (gridScope.autoRefresh) gridScope.retrieveData();
+                            $uibModalInstance.close();
+
+                            return data;
+                        },
+                        function (error) {
+                            $scope.$emit('tbForm_OnConnectionError', error);
+                            $rootScope.$broadcast('tbForm_OnConnectionError', error);
+                            $scope.Model.$isLoading = false;
+
+                            return error;
+                        });
+
+                    return result;
+                };
+
+                $scope.closePopup = function () {
+                    if (angular.isDefined($scope.Model.revertChanges)) {
+                        $scope.Model.revertChanges();
+                    }
+
+                    $uibModalInstance.close();
+                };
+            }
+        ]);
+})(angular);
