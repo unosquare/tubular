dd2a0c8691b2b857fc9d03ea41bec29f7ea381d2
greciaveronica <greciaveronica@gmail.com>
2017-03-29 13:54:38 -0600

    Correcting observations

--- src/Unosquare.Tubular/Javascript/tubular/services/editor.spec.js
+++ src/Unosquare.Tubular/Javascript/tubular/services/editor.spec.js
@@ -39,22 +39,22 @@ describe('Module: tubular.services', () => {
         });
 
         it('should show isvalid info with required = false', () => {
-            expect(scope.state.$valid()).toBe(true), 'should be false without value';
-            expect(scope.state.$errors.length).toBe(0), 'should not have error message';
+            expect(scope.state.$valid()).toBe(true, 'should be false without value');
+            expect(scope.state.$errors.length).toBe(0, 'should not have error message');
 
             scope.value = 'My component have a value';
-            expect(scope.state.$valid()).toBe(true), 'should be false with value';
-            expect(scope.state.$errors.length).toBe(0), 'should not have error message';
+            expect(scope.state.$valid()).toBe(true, 'should be false with value');
+            expect(scope.state.$errors.length).toBe(0, 'should not have error message');
         });
 
         it('should show isvalid info', () => {
             scope.required = true;
-            expect(scope.state.$valid()).toBe(false), 'should be false without value and required = true';
-            expect(scope.state.$errors.length).not.toBeLessThan(0), 'should have error message';
+            expect(scope.state.$valid()).toBe(false, 'should be false without value and required = true');
+            expect(scope.state.$errors.length).not.toBeLessThan(0, 'should have error message');
 
             scope.value = 'My component have a value';
-            expect(scope.state.$valid()).toBe(true), 'should be false with value';
-            expect(scope.state.$errors.length).toBe(0), 'should not have error message';
+            expect(scope.state.$valid()).toBe(true, 'should be false with value');
+            expect(scope.state.$errors.length).toBe(0, 'should not have error message');
         });
 
         it('should have getFormField defined', () => {
