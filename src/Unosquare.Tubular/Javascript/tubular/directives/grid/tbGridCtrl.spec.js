20ce827dbbad176296464d05d930a1e7f44c9817
greciaveronica <greciaveronica@gmail.com>
2017-03-31 16:15:09 -0600

    Updating grid controller spec

--- src/Unosquare.Tubular/Javascript/tubular/directives/grid/tbGridCtrl.spec.js
+++ src/Unosquare.Tubular/Javascript/tubular/directives/grid/tbGridCtrl.spec.js
@@ -2,20 +2,18 @@
 
 describe('Module: tubular.directives', function () {
     describe('Controller: tbGridController', function () {
-        var sut, scope, localStorageService, tubularPopupService, tubularModel, tubularHttp, $routeParams;
+        var sut, scope, window, tubularPopupService, tubularModel, tubularHttp, $routeParams;
         var $controller;
 
         beforeEach(function () {
             module('tubular.directives');
            
             module(function ($provide) {
-                localStorageService = jasmine.createSpyObj('localStorageService', ['get']);
                 tubularPopupService = jasmine.createSpyObj('tubularPopupService', ['m']);
                 tubularHttp = jasmine.createSpyObj('tubularHttp', ['getDataService', 'setRequireAuthentication']);
                 tubularModel = jasmine.createSpy();
                 $routeParams = jasmine.createSpyObj('$routeParams', ['name']);
                 
-                $provide.value('localStorageService', localStorageService);
                 $provide.value('tubularHttp', tubularHttp);
                 $provide.value('tubularModel', tubularModel);
                 $provide.value('tubularPopupService', tubularPopupService);
@@ -25,9 +23,10 @@ describe('Module: tubular.directives', function () {
 
         })
 
-        beforeEach(inject(function (_$controller_, $rootScope) {
+        beforeEach(inject(function (_$controller_, $rootScope, $window) {
             scope = $rootScope.$new();
             $controller = _$controller_;
+            window = $window;
             
         }));
 
