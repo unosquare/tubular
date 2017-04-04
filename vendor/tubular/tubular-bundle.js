22aa1bba225e0260be1ca76a3fa13c89769534fe
Travis CI <geovanni.perez@gmail.com>
2017-04-04 19:43:14 +0000

    Deploy to GitHub Pages: b5d77f7f85bca6373448398e28c9cd076e90d923

--- vendor/tubular/tubular-bundle.js
+++ vendor/tubular/tubular-bundle.js
@@ -4472,6 +4472,12 @@ angular.module('tubular.directives').run(['$templateCache', function ($templateC
                                 return;
                             }
 
+                            if ((val.DataType === 'date' || val.DataType === 'datetime' || val.DataType === 'datetimeutc')
+                                && !($ctrl.filter.Text === '' || $ctrl.filter.Text == null))
+                            {
+                                $ctrl.filter.Text = new Date($ctrl.filter.Text);
+                            }
+
                             if ($ctrl.filter.HasFilter !== val.Filter.HasFilter) {
                                 $ctrl.filter.HasFilter = val.Filter.HasFilter;
                                 $ctrl.filter.Text = val.Filter.Text;
