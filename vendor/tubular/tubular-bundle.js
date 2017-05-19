83dc95932dda5dfef5243b76966180dddfe8e62c
Travis CI <geovanni.perez@gmail.com>
2017-05-19 18:41:18 +0000

    Deploy to GitHub Pages: 0aae9412f3cee5526f3a76ec125e8c70242f4d7b

--- vendor/tubular/tubular-bundle.js
+++ vendor/tubular/tubular-bundle.js
@@ -2587,6 +2587,55 @@ angular.module('tubular.directives').run(['$templateCache', function ($templateC
         }]);
 })(angular, moment);
 
+(angular => {
+    'use strict';
+
+    angular.module('tubular.models')
+        /**
+         * @ngdoc factory
+         * @name tubularColumn
+         * @module tubular.models
+         *
+         * @description
+         * The `tubularColumn` factory is the base to generate a column model to use with `tbGrid`.
+         */
+        .factory('tubularColumn', [function() {
+            return (columnName, options) => {
+                options = options || {};
+                
+                const obj = {
+                    Label: options.Label  || (columnName || '').replace(/([a-z])([A-Z])/g, '$1 $2'),
+                    Name: columnName,
+                    Sortable: options.Sortable,
+                    SortOrder: parseInt(options.SortOrder) || -1,
+                    SortDirection: function(){
+                        if (angular.isUndefined(options.SortDirection)) {
+                            return 'None';
+                        }
+
+                        if (options.SortDirection.toLowerCase().indexOf('asc') === 0) {
+                            return 'Ascending';
+                        }
+
+                        if (options.SortDirection.toLowerCase().indexOf('desc') === 0) {
+                            return 'Descending';
+                        }
+
+                        return 'None';
+                    }(),
+                    IsKey: angular.isDefined(options.IsKey) ? options.IsKey : false,
+                    Searchable: angular.isDefined(options.Searchable) ? options.Searchable : false,
+                    Visible: options.Visible === 'false' ? false : true,
+                    Filter: null,
+                    DataType: options.DataType || 'string',
+                    Aggregate: options.Aggregate || 'none'
+                };
+
+                return obj;
+            };
+        }]);
+})(angular);
+
 
 (function(angular) {
 'use strict';
