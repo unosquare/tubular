4893c08c11dee4e83d0970a0887e798741968aed
alexey.turlapov <alexey.turlapov@unosquare.com>
2017-05-19 13:16:47 -0500

    fixing syntax error

--- test/e2e/grid/columns-as-attribute.spec.js
+++ test/e2e/grid/columns-as-attribute.spec.js
@@ -3,44 +3,45 @@
 
 describe('Component: Grid', () => {
 
-      var scope, compile, templateCache, template, element, TbColumn, $httpBackend;
+  var scope, compile, templateCache, template, element, TbColumn, $httpBackend;
 
 
-      beforeEach(module('tubular.tests'));
-      beforeEach(module('tubular'));
-
-
-
-      beforeEach(inject(function(_$compile_, _$rootScope_, _$templateCache_, _$httpBackend_, _tubularColumn_) {
-        scope = _$rootScope_;
-        compile = _$compile_;
-        templateCache = _$templateCache_;
-        $httpBackend = _$httpBackend_;
-        generate('columns-as-attribute');
-      }));
-
-      function generate(caseName) {
-        var tpl = templateCache.get(caseName + '.case.html');
-        template = angular.element(tpl);
-        element = compile(template)(scope);
-        scope.columns = [
-          new TbColumn("id", {
-            "Label": "ID"
-          }),
-          new TbColumn("name", {
-            "Label": "Nombre"
-          })
-        ]
-        scope.$digest();
-      }
+  beforeEach(module('tubular.tests'));
+  beforeEach(module('tubular'));
 
 
 
-      it('should pass', () => {
+  beforeEach(inject(function(_$compile_, _$rootScope_, _$templateCache_, _$httpBackend_, _tubularColumn_) {
+    scope = _$rootScope_;
+    compile = _$compile_;
+    templateCache = _$templateCache_;
+    $httpBackend = _$httpBackend_;
+    generate('columns-as-attribute');
+  }));
+
+  function generate(caseName) {
+    var tpl = templateCache.get(caseName + '.case.html');
+    template = angular.element(tpl);
+    element = compile(template)(scope);
+    scope.columns = [
+      new TbColumn("id", {
+        "Label": "ID"
+      }),
+      new TbColumn("name", {
+        "Label": "Nombre"
+      })
+    ]
+    scope.$digest();
+  }
 
 
-          const headers = element.find("th");
 
-          expect(headers).toBeDefined('should have columns');
-          expect(headers.length).toBe(2, 'should have 2 columns');
-      });
+  it('should pass', () => {
+
+
+    const headers = element.find("th");
+
+    expect(headers).toBeDefined('should have columns');
+    expect(headers.length).toBe(2, 'should have 2 columns');
+  });
+});
