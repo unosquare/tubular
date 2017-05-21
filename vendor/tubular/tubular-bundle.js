53819e6b9ab1f530a470672231b4bbe4af15a610
Travis CI <geovanni.perez@gmail.com>
2017-05-21 22:37:33 +0000

    Deploy to GitHub Pages: 7838b0c080ffc994b41870b40e290d7d2a164f96

--- vendor/tubular/tubular-bundle.js
+++ vendor/tubular/tubular-bundle.js
@@ -165,7 +165,7 @@
                     },
                     controller: ['$scope', 'tubularColumn',
                         function ($scope, tubularColumn) {
-                            $scope.$component = $scope.$parent.$component;
+                            $scope.$component = $scope.$parent.$component || $scope.$parent.$parent.$component;
                             $scope.tubularDirective = 'tubular-column';
 
                             $scope.sortColumn = multiple => $scope.$component.sortColumn($scope.column.Name, multiple);
@@ -2687,7 +2687,7 @@ angular.module('tubular.services', ['ui.bootstrap'])
       $httpProvider.interceptors.push('tubularAuthInterceptor');
       $httpProvider.interceptors.push('tubularNoCacheInterceptor');
     }
-  ])
+  ]);
 
 
 })(angular);
@@ -3713,7 +3713,7 @@ function exportToCsv(header, rows, visibility) {
                         \t\t<tb-column name="${el.Name}" label="${el.Label}" column-type="${el.DataType}" sortable="${el.Sortable}" 
                         \t\t\tis-key="${el.IsKey}" searchable="${el.Searchable}" ${el.Sortable ? `\r\n\t\t\tsort-direction="${el.SortDirection}" sort-order="${el.SortOrder}" ` : ' '}
                                 visible="${el.Visible}">${el.Filter ? '\r\n\t\t\t<tb-column-filter></tb-column-filter>' : ''}
-                        \t\t\t<tb-column-header>{{label}}</tb-column-header>
+                        \t\t\t<tb-column-header><span>{{label}}</span></tb-column-header>
                         \t\t</tb-column>`, '');
 
                 /**
