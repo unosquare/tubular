c30a02ed65d944b6953cd9eee9bb5bcf507a0617
Israel-94 <arnulfo_israel@hotmail.com>
2017-06-02 14:59:04 -0500

    changes tbGridPager

--- test/e2e/grid/tbGridPager.spec.js
+++ test/e2e/grid/tbGridPager.spec.js
@@ -2,35 +2,23 @@
 
 describe('Component: Grid.Pager', () => {
 
-  let scope, compile, templateCache, template, element, tubularColumn, $httpBackend;
+  let scope, compile, templateCache, template, element, $httpBackend;
 
   const serverUrl = 'api/data';
 
   beforeEach(module('tubular.tests'));
   beforeEach(module('tubular'));
 
-  beforeEach(inject(function (_$compile_, _$rootScope_, _$templateCache_, _$httpBackend_, _tubularColumn_) {
+  beforeEach(inject(function (_$compile_, _$rootScope_, _$templateCache_, _$httpBackend_) {
     scope = _$rootScope_;
     compile = _$compile_;
     templateCache = _$templateCache_;
     $httpBackend = _$httpBackend_;
-    tubularColumn = _tubularColumn_;
     scope.serverUrl = serverUrl;
-    scope.columns = [
-      new tubularColumn("id", {
-        "Label": "ID",
-        "DataType": "numeric"
-      }),
-      new tubularColumn("name", {
-        "Label": "Name",
-        "DataType": "string",
-        "Sortable": true
-      })
-    ]
   }));
 
   function generate(flush) {
-    var tpl = templateCache.get('pager.case.html');
+    var tpl = templateCache.get('tbGridPager.case.html');
     template = angular.element(tpl);
     element = compile(template)(scope);
     if (flush) $httpBackend.flush();
@@ -45,17 +33,16 @@ describe('Component: Grid.Pager', () => {
   });
 
   fit('should have more pages', () => {
-    var payload = [];
+        var payload = [];
     for (var index = 0; index < 100; index++) {
       payload.push([index, 'Name' + index]);
     }
     $httpBackend.expectPOST(serverUrl)
-      .respond(200, {"Counter": 0, "Payload": payload, "TotalRecordCount": 100, "FilteredRecordCount": 1, "TotalPages": 10, "CurrentPage": 1, "AggregationPayload": {}});
+      .respond(200, {"Counter": 0, "Payload": payload, "TotalRecordCount": 100, "FilteredRecordCount": 100, "TotalPages": 10, "CurrentPage": 1, "AggregationPayload": {}});
 
     generate(true);
 
     const buttons = element.find("li");
     expect(buttons.length).toBe(9, 'should have 9 buttons');
-
   });
 });
