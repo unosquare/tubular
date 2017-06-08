ce05285f152bf7b1219e9979d4abf70c6ba9a880
Geo Perez <geovanni.perez@gmail.com>
2017-06-08 11:15:35 -0500

    More Karma tests

--- test/e2e/grid/remote-data-json.spec.js
+++ test/e2e/grid/remote-data-json.spec.js
@@ -5,10 +5,16 @@ describe('Component: Grid', () => {
   let scope, compile, templateCache, template, element, $httpBackend;
   const serverUrl = 'sample.json';
 
-  const dataSource = [
+  const dataSourceWithArrays = [
     [1, 'Alexei'],
     [2, 'Alejandro'],
     [3, 'Geovanni']
+  ];
+
+  const dataSourceWithObjects = [
+    { ID: 1, Name: 'Alexei'},
+    { ID: 2, Name: 'Alejandro'},
+    { ID: 3, Name: 'Geovanni'}
   ];
 
   beforeEach(module('tubular.tests'));
@@ -30,17 +36,39 @@ describe('Component: Grid', () => {
   }
 
   it('remote data (json): should render rows correctly', () => {
-
     $httpBackend.expectPOST(serverUrl)
-      .respond(200, dataSource);
+      .respond(200, dataSourceWithArrays);
 
     generate();
-
     $httpBackend.flush();
 
     const headers = element.find("th");
     expect(headers.length).toBe(2, 'should have 2 columns');
     const dataRow = element.find("tbody tr");
     expect(dataRow.length).toBe(3, 'should have 3 data rows');
+  });
+
+  it('remote data (array of arrays json): should render cells correctly', () => {
+    $httpBackend.expectPOST(serverUrl)
+      .respond(200, dataSourceWithArrays);
+
+    generate();
+    $httpBackend.flush();
+
+    const dataCells = element.find("tbody tr td");
+    expect($j(dataCells[0]).text().trim()).toBe('1');
+    expect($j(dataCells[1]).text().trim()).toBe('Alexei');
+  });
+  
+  it('remote data (array of objects json): should render cells correctly', () => {
+    $httpBackend.expectPOST(serverUrl)
+      .respond(200, dataSourceWithObjects);
+
+    generate();
+    $httpBackend.flush();
+
+    const dataCells = element.find("tbody tr td");
+    expect($j(dataCells[0]).text().trim()).toBe('1');
+    expect($j(dataCells[1]).text().trim()).toBe('Alexei');
   });
 });
